cc571adf27d54c3049c8bafb6c5f5fa8556ba5c7
Limoges_Cyril <lcyril33@gmail.com>
2022-07-29 11:14:40 +0200

    script Js index.html dynamic integration  & factorisation

--- front/js/script.js
+++ front/js/script.js
@@ -1,28 +1,25 @@
-// DOM create elements
+// Cards <a> & <article> createElement = old method
 
-let cardLink = document.createElement("a");
-document.getElementById("items").appendChild(cardLink);
-cardLink.setAttribute("href", "./product.html?id=42");
-cardLink.className = "card-link";
-cardLink.innerHTML = "<article></article>";
+// const cardLink = document.createElement("a");
+// document.getElementById("items").appendChild(cardLink);
+// cardLink.setAttribute("href", "./product.html?id=42");
+// cardLink.className = "card-link";
+//cardLink.innerHTML = "<article></article>";
 
-let article = document.getElementsByTagName("article");
-article[0].innerHTML =
-  "<img src='' alt=''/>" +
-  "<h3 class='productName'></h3>" +
-  "<p class='productDescription'>lorem ipsum</p>";
+//let article = document.getElementsByTagName("article");
+//article[0].innerHTML = "<p class='productDescription'>lorem ipsum</p>";
 
-/**
- * request HTTP using fetch api
- * @param { string } resolve
- * @return { Promise } json
- */
+//const nameValue = document.querySelector(".productName");
+//const descriptionValue = document.querySelector(".productDescription");
+//const article = document.getElementsByTagName("article");
 
 const url = "http://localhost:3000/api/products";
 
-const nameValue = document.querySelector(".productName");
-const descriptionValue = document.querySelector(".productDescription");
-const imgValue = document.querySelector("article > img");
+/**
+ * request HTTP using fetch api
+ * @param { string } response
+ * @return { Promise } json
+ */
 
 let request = fetch(url)
   .then((res) => {
@@ -31,16 +28,45 @@ let request = fetch(url)
     }
   })
   .then((value) => {
-    console.log(value);
+    //console.log(value);
     for (i = 0; i < value.length; i++) {
-      //for (let product of value) {
-      nameValue.innerHTML = `${value[i].name}`;
-      descriptionValue.innerHTML = `${value[i].description}`;
-      imgValue.setAttribute(
-        `"src",${value[i].imageUrl}; "alt", ${value[i].altTxt}`
-      );
+      //for (let product of value)
+      const cardLink = document.createElement("a");
+      const article = document.createElement("article");
+      const headings = document.createElement("h3");
+      const Description = document.createElement("p");
+      const productImg = document.createElement("img");
+
+      // Product card
+      document.getElementById("items").appendChild(cardLink);
+      cardLink.href = "./product.html?id=42";
+
+      // Product article
+      cardLink.appendChild(article);
+
+      // Products images
+      productImg.src = value[i].imageUrl;
+      productImg.alt = value[i].altTxt;
+      article.appendChild(productImg);
+
+      // Products Headings
+      headings.innerHTML = value[i].name;
+      article.appendChild(headings);
+      //nameValue.innerHTML = value[i].name;
+
+      // Product description
+      Description.innerHTML = value[i].description;
+      article.appendChild(Description);
+      //descriptionValue.innerHTML = value[i].description;
     }
   })
   .catch((err) => {
     console.error("Error:", err);
   });
+
+// Event redirection on product page
+
+// document.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   location.href = "http://127.0.0.1:5500/front/html/product.html?id=42";
+// });
