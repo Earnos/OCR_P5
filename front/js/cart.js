d1d56ab6cd66e8ea6931a50acb80e88b9fa9587b
Limoges_Cyril <lcyril33@gmail.com>
2022-08-11 12:51:42 +0200

    display cart's products

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,81 +1,96 @@
-// function getAllItems() {
-//   for (i = 0; i < storage.length; i++)
-//     //storage = JSON.parse(localStorage.getItem("localStorage")) || [];
-//     localStorage.getItem(storage, JSON.stringify(storage[i]));
-//   console.log(storage);
-// }
-
 const productValues = JSON.parse(localStorage.getItem("storage"));
 console.log(productValues);
 
-const url = `http://localhost:3000/api/products/${productValues.at(0).id}`;
+for (i = 0; i < productValues.length; i++) {
+  // dynamic product's ID function
+  const articleID = getProductId();
+  function getProductId() {
+    return productValues[i].id;
+  }
 
-// Product Cart Article
-const cartItems = document.getElementById("cart__items");
-const cartArticle = document.createElement("article");
-const articleImgTag = document.createElement("div");
-const articleImg = document.createElement("img");
-const cardContent = document.createElement("div");
-const cardDescription = document.createElement("div");
-const descriptionTitle = document.createElement("h2");
-const descriptionTextColor = document.createElement("p");
-const descriptionTextPrice = document.createElement("p");
-const cartSettings = document.createElement("div");
-const cartQtySettings = document.createElement("div");
-const cartDelete = document.createElement("div");
-const cartQtyText = document.createElement("p");
-const cartQtyInput = document.createElement("input");
-const cartDeleteText = document.createElement("p");
-// const articleAttr = document.createAttribute("data-id");
-// articleAttr.value = `${productValues[0]}`;
-// let id = productValues.find(
-//   (identification) => productValues.id === `${productID}`
-// );
+  // data-color & data-id
+  const cartArticle = document.createElement("article");
+  cartArticle.setAttribute("data-id", productValues[i].id); // <-----------dynamic value
+  cartArticle.setAttribute("data-color", productValues[i].colors); // <-----------dynamic value
 
-cartArticle.className = "cart__item";
-cartArticle.setAttribute("data-id", productValues.at(0).id); // <-----------valeur dynamique
-cartArticle.setAttribute("data-color", productValues.at(1).colors); // <-----------valeur dynamique
-cartItems.appendChild(cartArticle);
+  // LocalStorage values creation&display
+  const descriptionTextColor = document.createElement("p");
+  descriptionTextColor.innerHTML = productValues[i].colors; // <-----------dynamic value
+  const cartQtyInput = document.createElement("input");
+  cartQtyInput.value = productValues[i].quantity; // <-----------dynamic value
 
-// image Tag article
-cartArticle.appendChild(articleImgTag);
-articleImgTag.className = "cart__item__img";
-articleImgTag.appendChild(articleImg);
-articleImg.src = "#"; // <-----------valeur dynamique
-articleImg.alt = ""; // <-----------valeur dynamique
+  // fecth request API in Loop
+  const url = `http://localhost:3000/api/products/${articleID}`;
+  fetch(url)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+    })
+    .then((value) => {
+      // create HTML structure
+      const cartItems = document.getElementById("cart__items");
+      const articleImgTag = document.createElement("div");
+      const articleImg = document.createElement("img");
+      const cardContent = document.createElement("div");
+      const cardDescription = document.createElement("div");
+      const descriptionTitle = document.createElement("h2");
+      const descriptionTextPrice = document.createElement("p");
+      const cartSettings = document.createElement("div");
+      const cartQtySettings = document.createElement("div");
+      const cartDelete = document.createElement("div");
+      const cartQtyText = document.createElement("p");
+      const cartDeleteText = document.createElement("p");
 
-// div tag cart item content
-cartArticle.appendChild(cardContent);
-cardContent.className = "cart__item__content";
+      cartArticle.className = "cart__item";
+      cartItems.appendChild(cartArticle);
 
-// div tag cart item description
-cardContent.appendChild(cardDescription);
-cardDescription.className = "cart__item__content__description";
-cardDescription.appendChild(descriptionTitle);
-cardDescription.appendChild(descriptionTextColor);
-cardDescription.appendChild(descriptionTextPrice);
+      // image Tag article
+      cartArticle.appendChild(articleImgTag);
+      articleImgTag.className = "cart__item__img";
+      articleImgTag.appendChild(articleImg);
+      articleImg.src = value.imageUrl; // <-----------dynamic value
+      articleImg.alt = value.altTxt; // <-----------dynamic value
 
-// div tag cart item settings
-cardContent.appendChild(cartSettings);
-cartSettings.className = "cart__item__content__settings";
-// settings quantity
-cartSettings.appendChild(cartQtySettings);
-cartQtySettings.className = "cart__item__content__settings__quantity";
-cartQtySettings.appendChild(cartQtyText);
-cartQtyText.innerHTML = "Quantité :"; // <-----------valeur dynamique
+      // div tag cart item content
+      cartArticle.appendChild(cardContent);
+      cardContent.className = "cart__item__content";
 
-// settings quantity input
-cartQtySettings.appendChild(cartQtyInput);
-cartQtyInput.type = "number";
-cartQtyInput.className = "itemQuantity";
-cartQtyInput.name = "itemQuantity";
-cartQtyInput.min = "1";
-cartQtyInput.max = "100";
-cartQtyInput.value = productValues.at(2).quantity; // <-----------valeur dynamique
+      // div tag cart item description
+      cardContent.appendChild(cardDescription);
+      cardDescription.className = "cart__item__content__description";
+      cardDescription.appendChild(descriptionTitle);
+      cardDescription.appendChild(descriptionTextColor);
+      cardDescription.appendChild(descriptionTextPrice);
+      descriptionTitle.innerHTML = value.name; // <-----------dynamic value
+      descriptionTextPrice.innerHTML = `${value.price}` + "€"; // <-----------dynamic value
 
-// settings delete
-cartSettings.appendChild(cartDelete);
-cartDelete.className = "cart__item__content__settings__delete";
-cartDelete.appendChild(cartDeleteText);
-cartDeleteText.className = "deleteItem";
-cartDeleteText.innerHTML = "Supprimer";
+      // div tag cart item settings
+      cardContent.appendChild(cartSettings);
+      cartSettings.className = "cart__item__content__settings";
+
+      // settings quantity
+      cartSettings.appendChild(cartQtySettings);
+      cartQtySettings.className = "cart__item__content__settings__quantity";
+      cartQtySettings.appendChild(cartQtyText);
+      cartQtyText.innerHTML = "Quantité :";
+
+      // settings delete
+      cartSettings.appendChild(cartDelete);
+      cartDelete.className = "cart__item__content__settings__delete";
+      cartDelete.appendChild(cartDeleteText);
+      cartDeleteText.className = "deleteItem";
+      cartDeleteText.innerHTML = "Supprimer";
+
+      // settings quantity input
+      cartQtySettings.appendChild(cartQtyInput);
+      cartQtyInput.type = "number";
+      cartQtyInput.className = "itemQuantity";
+      cartQtyInput.name = "itemQuantity";
+      cartQtyInput.min = "1";
+      cartQtyInput.max = "100";
+    })
+    .catch((err) => {
+      console.error("Error:", err);
+    });
+}
