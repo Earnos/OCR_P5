50a2b521b3142d73632193ece9b9687f17d8f1f7
Limoges_Cyril <lcyril33@gmail.com>
2022-08-17 18:08:59 +0200

    total quantity set

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,31 +1,58 @@
 const productValues = JSON.parse(localStorage.getItem("storage"));
 console.log(productValues);
 
+let totalPrice = 0;
+let totalQty = 0;
+
 // Remove cart's item
 const delCartItem = () => {
   const deleteSelection = document.querySelectorAll(".deleteItem");
-  for (i = 0; i < deleteSelection.length; i++) {
-    const deleteItem = deleteSelection[i];
+  for (let i = 0; i < deleteSelection.length; i++) {
+    const deleteItem = deleteSelection[i]; // Permet de stocker la ou les valeurs de mon array dans une variable et on obtien un objet.
     deleteItem.addEventListener("click", () => {
-      const indexToRemove = productValues.findIndex(
-        (element) => element === productValues
+      const selectId = deleteItem.closest("[data-id]").dataset.id;
+      const selectColor = deleteItem.closest("[data-color]").dataset.color;
+      console.log(selectId);
+      console.log(selectColor);
+
+      const toRemove = productValues.find(
+        (element) => element === selectId || selectColor
+        // element === deleteItem.closest("[data-id]").dataset.id &&
+        // deleteItem.closest("[data-color]").dataset.color
       );
-      console.log("click");
+      console.log(toRemove);
+      console.log(deleteItem);
+      // let dataColor = document.querySelectorAll("[data-color]");
+      // let dataId = document.querySelectorAll("[data-id]");
+      // if (deleteItem === toRemove) {
+      // let index = productValues.indexOf(i);
+      // productValues.splice(index, 1);
+      productValues.splice(toRemove, 3);
+      console.log(productValues.splice(toRemove, 3));
+      //localStorage.setItem("products", JSON.stringify(productValues));
+      //cartArticle.removeChild(article);
 
-      // let target = e.target;
-      // value.id = e.target;
-      productValues.splice(indexToRemove, 3);
-      console.log(productValues.splice(indexToRemove, 3));
+      //}
+      // data.datasets.splice(removalIndex, 1);
+      //   data.datasets.find((dataset, index) => {
+      //     if (dataset.id === 'myId') {
+      //        data.datasets.splice(index, 1);
+      //        return true; // stop searching
+      //     }
+      // });
+      // myChart.update();
+      // console.log(selectId);
+      // console.log(selectColor);
 
-      // remove the cost of item from total
-      // total -= itemCost[n]
+      //window.location.reload();
     });
+
+    // remove the cost of item from total
+    // total -= itemCost[n]
   }
 };
 
-let totalPrice = 0;
-
-for (i = 0; i < productValues.length; i++) {
+for (let i = 0; i < productValues.length; i++) {
   // dynamic product's ID function
   const articleID = getProductId();
   function getProductId() {
@@ -61,34 +88,6 @@ for (i = 0; i < productValues.length; i++) {
   // productValues.find(() => productValues.colors === productValues.colors);
   // if (productValues[i].colors === productValues[i].colors) {
   //   productValues.quantity + productValues.quantity == cartQtyInput.value;
-
-  //   //let qtySum = 0;
-  //   productValues.find((color) => productValues[i].colors);
-  //const concatArray = productValues.concat(productValues);
-  //console.table(concatArray);
-  // let sumQty = productValues[i].quantity + productValues[i].quantity;
-  // sumQty = productValues[i].quantity;
-  //}
-  //setIncrementationColor();
-
-  //Remove cart's item
-  // const deleteSelection = document.querySelector(".deleteItem");
-  // console.log(deleteSelection);
-  // deleteSelection.addEventListener("click", (e) => {
-  //   productValues.findIndex((id) => id === id).splice(0, 3);
-  // });
-  // console.log(e.target);
-  // console.log("clicked");
-
-  //  function getTotal() {
-  //    let totalPrice = 0;
-
-  //    totalPrice += value.price * productValues[i].quantity;
-
-  //    document.getElementById("totalQuantity").innerHTML = "";
-  //    document.getElementById("totalPrice").innerHTML += totalPrice;
-  //  }
-  //  getTotal();
 
   // fecth request API in Loop
   const url = `http://localhost:3000/api/products/${articleID}`;
@@ -161,54 +160,21 @@ for (i = 0; i < productValues.length; i++) {
       cartQtyInput.min = "1";
       cartQtyInput.max = "100";
 
-      //console.log(e.target);
-
-      // Totals function
-      //function getTotal() {
-      //let totalPrice = 0;
-      //let totalArticle = 0;
-      //totalArticle =
-
-      //totalPrice = new Object();
+      // Totals quantities & prices
       totalPrice += parseInt(value.price) * parseInt(cartQtyInput.value);
-      //totalPrice.concat(totalPrice[0]);
-      //totalPrice.push(parseInt(value.price) * parseInt(cartQtyInput.value));
-      // arrSum = function(arr){  return arr.reduce(function(a,b){    return a + b  }, 0);}
-      // arrSum = function (totalPrice) {
-      //   return totalPrice.reduce(function (a, b) {
-      //     return a + b;
-      //   }, 0);
-      // };
-      // totalPrice.forEach((number) => {
-      // let sumOfPrice = 0;
-      // sumOfPrice += (number + number).valueOf();
-      // sumOfPrice = parseInt(sumOfPrice, 10);
-      // });
-      totalQty = 0;
-      totalQty = parseInt(totalQty);
-      totalQty = cartQtyInput.value;
-
-      //let SumQty = 0;
-      //Object.values(totalPrice);
-
-      //totalArray = [];
-      //totalArray.push(totalPrice);
-      //console.log(totalArray);
-
+      totalQty += parseInt(cartQtyInput.value);
       document.getElementById("totalQuantity").innerHTML = totalQty;
       document.getElementById("totalPrice").innerHTML = totalPrice;
-
       console.log(totalPrice);
       console.log(totalQty);
-      //}
-      //getTotal();
+
+      // for one iteration on loop i
+      if (i === productValues.length - 1) {
+        // permet de recuperer sur la derniere itération de ma boucle pour eviter que cette fonction boucle 4 fois
+        delCartItem();
+      }
     })
     .catch((err) => {
       console.error("Error:", err);
     });
-  // if (i === productValues.length - 1) {
-  //   delCartItem();
-  // } // permet de recuperer la derniere itération de ma boucle pour eviter que cette fonction boucle 4 fois
-  // console.log(i);
-  // console.log(productValues.length);
 }
